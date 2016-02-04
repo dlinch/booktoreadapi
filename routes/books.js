504b46c91aa4fdccc5a5cabd2600e9f2371f662b
dlinch <dlinch33@gmail.com>
2016-02-03 17:33:43 -0700

    api working, get all, get random, get random multiple award winner, and get by specific award winners working

---
+++ routes/books.js
@@ -0,0 +1,45 @@
+var express = require('express')
+var router = express.Router();
+var knex = require('../db/knex')
+
+var Book = function(){
+  return knex('book')
+}
+
+router.get('/', function(req, res){
+  Book().select().then(function(books){
+    res.json(books)
+
+  })
+})
+
+router.get('/random', function(req, res){
+  Book().select().then(function(books){
+
+    res.json(books[Math.floor(Math.random()*books.length)])
+  })
+})
+
+router.get('/randommultiple', function(req, res){
+  Book().select().then(function(books){
+
+    doubleFilter = function(object){
+       if (object.winner.length>1){
+         return true;
+       }
+     }
+    var doubleBooks= books.filter(doubleFilter)
+    res.json(doubleBooks[Math.floor(Math.random()*doubleBooks.length)])
+
+  })
+})
+
+router.get('/:award', function(req, res){
+  var award = []
+  award.push(req.params.award);
+  knex.raw("select * from book where winner @> '{"+req.params.award+"}'").then(function(books){
+    res.json(books)
+  })
+})
+
+module.exports = router;
